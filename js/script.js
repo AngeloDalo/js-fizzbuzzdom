1334a4f7ee373715a45f6bbff987f892ad5852c7
AngeloDalo <angelo251158@gmail.com>
2021-11-23 14:22:06 +0100

    container

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
 /**
  * stampare numeri da 1 a 100
  * multipli di 3 = Fizz
- * mulripli di 5 = buzz
+ * multipli di 5 = buzz
  * multipli di 3 e 5 FizzBuzz
  * appendere nel container DOM numero o stringa
  */
 
 //creare costante per il container
-const container = document.querySelector('.container');
+const container = document.querySelector('.container-number');
 
 for (let i=1; i<=100; i++) {
     //creazione box contenente il numero
     const element = document.createElement('div');
     element.classList.add('box');
     
-    //if
     if ((i%3==0) && (i%5==0)) {
         element.innerHTML= "FizzBuzz";
+        element.classList.add('fizzbuzz');
     } else if (i%5==0) {
         element.innerHTML = "Buzz";
+        element.classList.add('buzz');
     } else if (i%3==0) {
         element.innerHTML = "Fizz";
+        element.classList.add('fizz');
     } else {
         element.innerHTML = i;
+        element.classList.add('blu');
     }
     
-    
-    //element.innerHTML= i;
     container.append(element);
 }
