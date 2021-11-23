d6c7c47e484ba69932da7ec793366e425385efb2
AngeloDalo <angelo251158@gmail.com>
2021-11-23 15:06:32 +0100

    ordine

--- js/script.js
+++ js/script.js
@@ -9,18 +9,18 @@
 //creare costante per il container
 const container = document.querySelector('.container-number');
 
-for (let i=1; i<=100; i++) {
+for (let i = 1; i <= 100; i++) {
     //creazione box contenente il numero
     const element = document.createElement('div');
     element.classList.add('box');
     
-    if ((i%3==0) && (i%5==0)) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
         element.innerHTML= "FizzBuzz";
         element.classList.add('fizzbuzz');
-    } else if (i%5==0) {
+    } else if (i % 5 == 0) {
         element.innerHTML = "Buzz";
         element.classList.add('buzz');
-    } else if (i%3==0) {
+    } else if (i % 3 == 0) {
         element.innerHTML = "Fizz";
         element.classList.add('fizz');
     } else {
